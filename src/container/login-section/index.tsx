7d1975c95b900cbcf244883881599b353f0c9d2a
Vincent Roberge-Dubois <vincent.robergedubois@gmail.com>
2017-12-24 09:07:21 -0500

    Correcting login view adding login-section container

---
+++ src/container/login-section/index.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+
+import * as H from "history";
+import { Column, Row } from "react-foundation";
+import { connect } from "react-redux";
+import { bindActionCreators, Dispatch } from "redux";
+
+import { loadMenuItemList } from "action/menu-item-action";
+import { loadUser } from "action/user-action";
+import { IState } from "model/state";
+
+interface ILoginSectionOwnProps {
+  history: H.History;
+}
+
+interface ILoginSectionDispatchProps {
+  loadMenuItemList: typeof loadMenuItemList;
+  loadUser: typeof loadUser;
+}
+
+type TLoginSectionProps = ILoginSectionOwnProps & ILoginSectionDispatchProps;
+
+class LoginSection extends React.PureComponent<TLoginSectionProps, {}> {
+  public render() {
+    return (
+      <form onSubmit={this.handleLogin()}>
+        <Row>
+          <Column large={12}>
+            <label>Nom d'utilisateur</label>
+            <input name="username" type="text" />
+          </Column>
+        </Row>
+        <Row>
+          <Column large={12}>
+            <label>Mot de passe</label>
+            <input name="password" type="password" />
+          </Column>
+        </Row>
+        <button type="submit" className="button expanded">Connexion</button>
+      </form>
+    );
+  }
+
+  private handleLogin = () => (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    this.props.loadUser(1);
+    this.props.loadMenuItemList(1, 1);
+    this.props.history.push("/tools");
+  }
+}
+
+const mapDispatchToProps = (dispatch: Dispatch<IState>): ILoginSectionDispatchProps => {
+  return {
+    loadMenuItemList: bindActionCreators(loadMenuItemList, dispatch),
+    loadUser: bindActionCreators(loadUser, dispatch),
+  };
+};
+
+export default connect<{}, ILoginSectionDispatchProps, {}>(null, mapDispatchToProps)(LoginSection);
--- src/view/login/index.tsx
+++ src/view/login/index.tsx
@@ -1,24 +1,12 @@
 import * as React from "react";
 
 import { Alignments, Column, Row } from "react-foundation";
-import { connect } from "react-redux";
 import { RouteComponentProps } from "react-router-dom";
-import { bindActionCreators, Dispatch } from "redux";
-
-import { loadMenuItemList } from "action/menu-item-action";
-import { loadUser } from "action/user-action";
-import { IState } from "model/state";
 
 import SideBar from "component/side-bar";
+import LoginSection from "container/login-section";
 
-interface ILoginDispatchProps {
-  loadMenuItemList: typeof loadMenuItemList;
-  loadUser: typeof loadUser;
-}
-
-type TLoginProps = RouteComponentProps<{}> & ILoginDispatchProps;
-
-class Login extends React.PureComponent<TLoginProps, {}> {
+class Login extends React.PureComponent<RouteComponentProps<{}>, {}> {
   public render(): JSX.Element {
     return (
       <Row id="login-container" verticalAlignment={Alignments.MIDDLE} isExpanded={true}>
@@ -29,39 +17,11 @@ class Login extends React.PureComponent<TLoginProps, {}> {
           largeSize={6}
         />
         <Column className="form" large={6}>
-          <form onSubmit={this.handleLogin()}>
-            <Row>
-              <Column large={12}>
-                <label>Nom d'utilisateur</label>
-                <input name="username" type="text" />
-              </Column>
-            </Row>
-            <Row>
-              <Column large={12}>
-                <label>Mot de passe</label>
-                <input name="password" type="password" />
-              </Column>
-            </Row>
-            <button type="submit" className="button expanded">Connexion</button>
-          </form>
+          <LoginSection history={this.props.history} />
         </Column>
       </Row>
     );
   }
-
-  private handleLogin = () => (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    this.props.loadUser(1);
-    this.props.loadMenuItemList(1, 1);
-    this.props.history.push("/tools");
-  }
 }
 
-const mapDispatchToProps = (dispatch: Dispatch<IState>): ILoginDispatchProps => {
-  return {
-    loadMenuItemList: bindActionCreators(loadMenuItemList, dispatch),
-    loadUser: bindActionCreators(loadUser, dispatch),
-  };
-};
-
-export default connect<{}, ILoginDispatchProps, {}>(null, mapDispatchToProps)(Login);
+export default Login;
